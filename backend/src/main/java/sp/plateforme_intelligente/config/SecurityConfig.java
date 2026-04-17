54affc025967097763bf3f065010b155d494b2c4
Elmehdi <Elmehdi@gmail.com>
2026-04-17 18:32:38 +0100

    Modif Security

--- backend/src/main/java/sp/plateforme_intelligente/config/SecurityConfig.java
+++ backend/src/main/java/sp/plateforme_intelligente/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -36,14 +37,10 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .cors().disable()
-            .csrf().disable()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .authorizeHttpRequests()
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
                 .requestMatchers(
+                    "/api/auth/**",
                     "/",
                     "/index.html",
                     "/favicon.ico",
@@ -53,8 +50,17 @@ public class SecurityConfig {
                     "/*.png",
                     "/*.json",
                     "/*.woff2"
-                ).permitAll()
-                .requestMatchers("/api/auth/**").permitAll()
+                );
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .cors().disable()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeHttpRequests()
                 .anyRequest().authenticated()
             .and()
             .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
